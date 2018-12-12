20324080b03b1bc7290504ee088fc7dc4d6c963a
rmlassesen <rmlassesen@gmail.com>
2018-12-12 21:55:32 +0100

    new file:   lib/__pycache__/rf_handler.cpython-35.pyc 	new file:   lib/__pycache__/server.cpython-35.pyc 	modified:   lib/rf_handler.py

--- lib/rf_handler.py
+++ lib/rf_handler.py
@@ -2,14 +2,15 @@ import RPi.GPIO as gp
 import time
 
 def listen():
-    print("Listening for RF signals")
+    print("Listening for RF signals:")
     recieve()
     return True
 
 def recieve():
     gp.setmode(gp.BCM)
     gp.setup(16, gp.IN)
-
+    gp.setup([20,21], gp.OUT, initial=gp.HIGH)
+    
     count = 0
 
     while count != 400000000:
