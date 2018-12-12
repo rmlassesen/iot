d5948c8e9967fc5a86e87c115ef1680d6e6e3756
rmlassesen <rmlassesen@gmail.com>
2018-12-12 14:36:33 +0000

    Changes to be committed: 	modified:   lib/rf_handler.py

--- lib/rf_handler.py
+++ lib/rf_handler.py
@@ -1,3 +1,21 @@
+import RPi.GPIO as gp
+import time
+
 def listen():
     print("Listening for RF signals")
+    recieve()
     return True
+
+def recieve()
+    gp.setmode(gp.BCM)
+    gp.setup(16, gp.IN)
+
+    count = 0
+
+    while count != 400000000:
+        rcv = gp.input(16):
+        if rcv == 1:
+            count += 1
+            print(count)
+
+    gp.cleanup()
